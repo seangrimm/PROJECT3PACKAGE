1a158c60ece7f1e628564446db0644c8388e5449
Sean Grimm <sean.grimmm@gmail.com>
2021-03-13 20:50:53 -0800

    my_t.test documentation and tests

--- R/my_t.test.R
+++ R/my_t.test.R
@@ -1,6 +1,22 @@
 #' T-test function
 #'
-#' This function performs a t-test
+#' This function performs a t-test.
+#'
+#' @param x Numeric vector of data to be used in the t-test.
+#' @param alternative Character string indicating type of alternative
+#'   hypothesis. Only accepts "\code{two.sided}", "\code{less}" or
+#'   "\code{greater}".
+#' @param mu Numeric input indicating the value of the mean for the null
+#'   hypothesis.
+#'
+#' @return Returns a list containing the t-test test statistic, the degrees of
+#'   freedom used, the given \code{alternative}, and the resulting p-value.
+#'
+#' @examples
+#' my_t.test(c(1, 2, 3, 4, 5), "two.sided", 1)
+#' my_t.test(c(1, 2, 3, 4, 5), "less", 1)
+#'
+#' @export
 my_t.test <- function(x, alternative, mu) {
   # catch invalid parameters
   if (alternative != "two.sided" & alternative != "less" &
---
+++ tests/testthat/test-my_t.R
@@ -0,0 +1,12 @@
+test_that("T-test works", {
+  expect_equal(my_t.test(c(1, 2, 3, 4, 5), "two.sided", 1)[[1]], 2.828,
+               tolerance = .001)
+  expect_equal(my_t.test(c(1, 2, 3, 4, 5), "two.sided", 1)[[2]], 4)
+  expect_equal(my_t.test(c(1, 2, 3, 4, 5), "two.sided", 1)[[3]], "two.sided")
+  expect_equal(my_t.test(c(1, 2, 3, 4, 5), "two.sided", 1)[[4]], 0.0474,
+               tolerance = .0005)
+})
+test_that("Errors/messages thrown correctly", {
+  expect_error(my_t.test(c(1, 2, 3), "two.side", 1))
+  expect_error(my_t.test(c(1, 2, 3), "two.sided", "string"))
+})
