4676eab12c1bcd6c1fa5a463d875e7286ca09036
Sean Grimm <sean.grimmm@gmail.com>
2021-03-13 20:58:01 -0800

    additional my_t.test tests

--- tests/testthat/test-my_t.R
+++ tests/testthat/test-my_t.R
@@ -5,6 +5,10 @@ test_that("T-test works", {
   expect_equal(my_t.test(c(1, 2, 3, 4, 5), "two.sided", 1)[[3]], "two.sided")
   expect_equal(my_t.test(c(1, 2, 3, 4, 5), "two.sided", 1)[[4]], 0.0474,
                tolerance = .0005)
+  expect_equal(my_t.test(c(1, 2, 3, 4, 5), "greater", 1)[[4]], .0237,
+               tolerance = .0005)
+  expect_equal(my_t.test(c(1, 2, 3, 4, 5), "less", 1)[[4]], .9763,
+               tolerance = .0005)
 })
 test_that("Errors/messages thrown correctly", {
   expect_error(my_t.test(c(1, 2, 3), "two.side", 1))
