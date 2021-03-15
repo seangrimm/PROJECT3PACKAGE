92ef93501a337de996d145d859ce6fae9465dfb1
Sean Grimm <sean.grimmm@gmail.com>
2021-03-15 16:40:10 -0700

    my_pow, f_to_c, my_knn_cv tests

---
+++ tests/testthat/test-my_knn_cv.R
@@ -0,0 +1,10 @@
+test_that("proper output", {
+  expect_equal(my_knn_cv(train, cl, 5, 1)[[2]], 1)
+})
+
+test_that("errors received when appropriate", {
+  expect_error(my_knn_cv(2, na.omit(my_penguins)[1], 1, 5))
+  expect_error(my_knn_cv(na.omit(my_penguins)[,3:6], 2, 1, 5))
+  expect_error(my_knn_cv(na.omit(my_penguins)[,3:6], na.omit(my_penguins)[1],
+                         .1, 1))
+})
